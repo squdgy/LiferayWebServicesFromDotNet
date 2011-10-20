052d8a2ac5d32b6d79b3101aef5683307b82c790
Maura Wilder <maura@devpartners.com>
2011-10-19 20:49:58 -0400

    Initial Commit

---
+++ LifeRayWebServicesExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LifeRayWebServicesExample.LiferayUserService;
+
+namespace LifeRayWebServicesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new UserServiceSoapClient();
+            client.ClientCredentials.UserName.UserName = "2";
+            client.ClientCredentials.UserName.Password = "test";
+            var user = client.getUserByScreenName(1, "joebloggs");
+            Console.WriteLine(user.greeting);
+            Console.ReadLine();
+        }
+    }
+}
